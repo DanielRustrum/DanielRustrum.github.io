112860ef4802d64970351ffedcf7965ac4755d30
DanielRustrum <danieltrustrum@gmail.com>
2022-02-14 09:31:43 -0700

    Add Petco LP

---
+++ petco/data.js
@@ -0,0 +1,237 @@
+//TODO: Populate Dataset
+const data = {
+    products: {
+        'dog': {
+            link: 'https://www.petco.com/shop/en/petcostore/category/dog',
+            list: [
+                {
+                    name: "Dog Food 1",
+                    img: "./assets/food-bag.jpg",
+                    price: "15.00",
+                    link: "."
+                },{
+                    name: "Dog Food 2",
+                    img: "./assets/food-bag.jpg",
+                    price: "12.00",
+                    link: "."
+                },{
+                    name: "Dog Food 3",
+                    img: "./assets/food-bag.jpg",
+                    price: "16.00",
+                    link: "."
+                },{
+                    name: "Dog Food 4",
+                    img: "./assets/food-bag.jpg",
+                    price: "18.00",
+                    link: "."
+                },{
+                    name: "Dog Food 5",
+                    img: "./assets/food-bag.jpg",
+                    price: "15.30",
+                    link: "."
+                },{
+                    name: "Dog Food 6",
+                    img: "./assets/food-bag.jpg",
+                    price: "15.30",
+                    link: "."
+                },{
+                    name: "Dog Food 7",
+                    img: "./assets/food-bag.jpg",
+                    price: "15.30",
+                    link: "."
+                },
+            ],
+        },
+        'cat': {
+            link: '.',
+            list: [
+                {
+                    name: "Cat Food 1",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Cat Food 2",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Cat Food 3",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Cat Food 4",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },
+            ]
+        },
+        'fish': {
+            link: '.',
+            list: [
+                {
+                    name: "Fish Food 1",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Fish Food 2",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Fish Food 3",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },
+            ]
+        },
+        'small-pet': {
+            link: '.',
+            list: [
+                {
+                    name: "Small Pet Food 1",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Small Pet Food 2",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },
+            ]
+        },
+        'reptile': {
+            link: '.',
+            list: [
+                {
+                    name: "Reptile Food 1",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Reptile Food 2",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Reptile Food 3",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Reptile Food 4",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },
+            ]
+        },
+        'bird': {
+            link: '.',
+            list: [
+                {
+                    name: "Bird Food 1",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Bird Food 2",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Bird Food 3",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Bird Food 4",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },
+            ]
+        },
+        'all': {
+            link: '.',
+            list: [
+                {
+                    name: "Cat Food 1",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Bird Food 2",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Reptile Food 3",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Bird Food 4",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Cat Food 5",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Reptile Food 6",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Cat Food 7",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Cat Food 1",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Bird Food 2",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Reptile Food 3",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Bird Food 4",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Cat Food 5",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Reptile Food 6",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },{
+                    name: "Cat Food 7",
+                    img: "./assets/food-bag.jpg",
+                    price: "13.00",
+                    link: "."
+                },
+            ]
+        },
+    }
+}
