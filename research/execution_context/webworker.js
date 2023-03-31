253afdb55327ae91ef021a9eda64ca1dde22ab66
Daniel Rustrum <DanielRustrum@users.noreply.github.com>
2023-03-31 19:46:32 +0000

    changes

--- research/execution_context/webworker.js
+++ research/execution_context/webworker.js
@@ -1,27 +1,27 @@
 let var_a = "value"
 let var_b = "value"
 
-function a() {
-    let var_a = "value"
-    let var_c = "value"
-    debugger;
-}
-
 
 function strict_mode() {
     'use strict';
+
     function a() {
         let var_a = "value"
+        let var_c = "value"
         debugger;
     }
     
+    a()
 }
 
 function sloppy_mode() {
     function a() {
         let var_a = "value"
+        let var_c = "value"
         debugger;
     }
+
+    a()
 }
 
 strict_mode()
