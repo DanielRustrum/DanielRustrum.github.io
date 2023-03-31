c054ec9ebd819b0aee03b7e22e8fdb2bb3b6a3b1
Daniel Rustrum <DanielRustrum@users.noreply.github.com>
2023-03-31 19:52:03 +0000

    logging

--- research/execution_context/webworker.js
+++ research/execution_context/webworker.js
@@ -1,11 +1,13 @@
 let var_a = "value"
 let var_b = "value"
 
+console.log(this)
 
 function strict_mode() {
     'use strict';
 
     function a() {
+        console.log(a)
         let var_a = "value"
         let var_c = "value"
         debugger;
