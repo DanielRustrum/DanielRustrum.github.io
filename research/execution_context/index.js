2205f91f5da5b4512e9a500d4b5f43efab5074d7
Daniel Rustrum <DanielRustrum@users.noreply.github.com>
2023-03-31 18:06:07 +0000

    more changes

--- research/execution_context/index.js
+++ research/execution_context/index.js
@@ -1,5 +1,17 @@
-function strict() {
+function sloppy_mode() {
+
+}
+
+function strict_mode() {
     'use strict';
+
+	const var_a = "value"
+	let var_b;
+	debugger;
+
+	var var_c = "value"
+	var_b = "value"
+
 	function a() {
 		function b() {
 			function c() {}
@@ -8,6 +20,16 @@ function strict() {
 		b()
 	}
 	a()
+
+	const obj_a = {
+		a: this,
+		b: function() {
+			debugger;
+			return "value"
+		}
+	}
+
+	debugger;
 }
 
-strict()
+strict_mode()
