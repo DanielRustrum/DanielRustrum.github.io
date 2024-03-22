aeb901cb6cec0aa48cf95ab0eb07d2e5197a1141
DanielRustrum <danieltrustrum@gmail.com>
2024-03-22 14:12:12 -0700

    Script Src

--- global.js
+++ global.js
@@ -1,18 +1,20 @@
-let nav_dialog = document.querySelector("dialog")
-let nav_button = document.querySelector("nav > button")
-let close_nav_button = document.querySelector("#close-modal")
+document.addEventListener("include.load:nav", () => {
+    let nav_dialog = document.querySelector("dialog")
+    let nav_button = document.querySelector("nav > button")
+    let close_nav_button = document.querySelector("#close-modal")
 
-nav_button.addEventListener("click", event => {
-    nav_dialog.showModal()
-    event.preventDefault()
-})
+    nav_button.addEventListener("click", event => {
+        nav_dialog.showModal()
+        event.preventDefault()
+    })
 
-close_nav_button.addEventListener("click", event => {
-    nav_dialog.close()
-    event.preventDefault()
-})
+    close_nav_button.addEventListener("click", event => {
+        nav_dialog.close()
+        event.preventDefault()
+    })
 
-nav_dialog.addEventListener("click", event => {
-    if (nav_dialog !== event.target) return;
-    nav_dialog.close()
+    nav_dialog.addEventListener("click", event => {
+        if (nav_dialog !== event.target) return;
+        nav_dialog.close()
+    })
 })
