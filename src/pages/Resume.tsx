67fd5e3eaf16f0281d90a5d1729fdc62cd19cded
DanielRustrum <danieltrustrum@gmail.com>
2025-03-09 17:23:16 -0700

    changes

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,7 +1,6 @@
-import {Tile} from '../components/core/Tile'
 
 export function Resume() {
   return (
-    <Tile name="Test" />
+    <></>
   )
 }
--- src/pages/Story.tsx
+++ src/pages/Story.tsx
@@ -1,7 +1,6 @@
-import {Tile} from '../components/core/Tile'
 
 export function Story() {
   return (
-    <Tile name="Test" />
+    <></>
   )
 }
